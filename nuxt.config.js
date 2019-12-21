336365c4eeff88e3789dc0fe751dee0f2d9aaffc
Nafis Fuad <fuadbd14@gmail.com>
2019-12-21 16:55:32 +0600

    Remove duplicate link

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,39 @@
+import data from './static/storedata.json'
+const dynamicRoutes = () => {
+  return new Promise((resolve) => {
+    resolve(data.map((el) => `product/${el.id}`))
+  })
+}
+
 export default {
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
     title: process.env.npm_package_name || '',
+    script: [{ src: 'https://js.stripe.com/v3/' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Montserrat:300,600|PT+Serif&display=swap'
+      }
+    ]
+  },
+  generate: {
+    routes: dynamicRoutes
   },
   /*
    ** Customize the progress-bar color
@@ -23,33 +42,15 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['normalize.css', { src: '~/assets/main.scss', lang: 'sass' }],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
+  plugins: [`~/plugins/currency-filter.js`],
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
+  modules: [],
   /*
    ** Build configuration
    */
