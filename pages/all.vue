1cc5457f71f0db754b62d9510a17f7695a07e5d6
Nafis Fuad <fuadbd14@gmail.com>
2019-12-22 19:26:06 +0600

    Add all books page

---
+++ pages/all.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-store-grid :data="data" />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import AppStoreGrid from '~/components/AppStoreGrid.vue'
+
+export default {
+  components: {
+    AppStoreGrid
+  },
+  computed: {
+    ...mapState(['data'])
+  }
+}
+</script>
