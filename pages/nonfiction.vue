62c74e5600a2e07636b77c845f5a0727ec7aec81
Nafis Fuad <fuadbd14@gmail.com>
2019-12-22 19:24:55 +0600

    Add nonfiction page

---
+++ pages/nonfiction.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-store-grid :data="nonfiction" />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AppStoreGrid from '~/components/AppStoreGrid.vue'
+
+export default {
+  components: {
+    AppStoreGrid
+  },
+  computed: {
+    ...mapGetters(['nonfiction'])
+  }
+}
+</script>
