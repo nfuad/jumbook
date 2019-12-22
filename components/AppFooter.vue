ad928d06285237ee4aed7ba9bb7a9ecb02a11d11
Nafis Fuad <fuadbd14@gmail.com>
2019-12-22 21:53:26 +0600

    Add footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>
+      Made with love by
+      <a href="https://github.com/nfuad" target="_blank">Nafis Fuad</a>.
+      Twitter: <a href="https://twitter.com/sarah_edo">@___fuad</a>. This is an
+      open source project. Find the
+      <a href="https://github.com/nfuad/jumbook" target="_blank"
+        >source code here</a
+      >. <br />
+      Feel free to copy me.
+    </p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  align-items: center;
+  padding: 55px 0px;
+  background: #ffecd4;
+  margin-top: 65px;
+
+  p {
+    max-width: 30%;
+    margin: 0 auto;
+  }
+}
+</style>
