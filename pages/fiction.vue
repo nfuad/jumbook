114f1b6b5e8905b6ccdf29ac8ef99db4a1d1996a
Nafis Fuad <fuadbd14@gmail.com>
2019-12-22 19:25:29 +0600

    Add fiction page

---
+++ pages/fiction.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-store-grid :data="fiction" />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AppStoreGrid from '~/components/AppStoreGrid.vue'
+
+export default {
+  components: {
+    AppStoreGrid
+  },
+  computed: {
+    ...mapGetters(['fiction'])
+  }
+}
+</script>
